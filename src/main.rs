5c08c31b0aecf7529be3b1e5e2a59baefff6dc04
Rodolfo Ruiz <rpruiz@me.com>
2020-11-01 17:42:13 -0600

    Serialization and posts display

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,32 @@
 use actix_web::{App, HttpResponse, HttpServer, Responder, web};
+use serde::Serialize;
 use tera::{Context, Tera};
+
+#[derive(Serialize)]
+struct Post {
+    title: String,
+    link: String,
+    author: String,
+}
 
 async fn index(tera: web::Data<Tera>) -> impl Responder {
     let mut data = Context::new();
+
+    let posts = [
+        Post {
+            title: String::from("First link"),
+            link: String::from("https://link1.com"),
+            author: String::from("A"),
+        },
+        Post {
+            title: String::from("Second link"),
+            link: String::from("https://link2.com"),
+            author: String::from("B"),
+        },
+    ];
+
     data.insert("title", "Hacker Clone");
-    data.insert("name", "Rodolfo");
+    data.insert("posts", &posts);
 
     let rendered = tera.render("index.html", &data).unwrap();
     HttpResponse::Ok().body(rendered)

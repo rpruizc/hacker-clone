0bef251d0bb79f067334c7cba6db130af292c88f
Rodolfo Ruiz <rpruiz@me.com>
2020-11-01 17:02:19 -0600

    Tera templates added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpResponse, HttpServer, Responder, web};
+use tera::{Context, Tera};
+
+async fn index(tera: web::Data<Tera>) -> impl Responder {
+    let mut data = Context::new();
+    data.insert("title", "Hacker Clone");
+    data.insert("name", "Rodolfo");
+
+    let rendered = tera.render("index.html", &data).unwrap();
+    HttpResponse::Ok().body(rendered)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        let tera = Tera::new("templates/**/*").unwrap();
+        App::new()
+            .data(tera)
+            .route("/", web::get().to(index))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
